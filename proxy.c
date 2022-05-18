93d165fb5e18d73a09024c1c7efc2bf822e5619c
Ubuntu <ubuntu@ip-172-31-32-54.ap-northeast-2.compute.internal>
2022-05-18 10:42:59 +0000

    proxy 3

--- proxy.c
+++ proxy.c
@@ -22,8 +22,23 @@ static const char *host_key = "Host";
 void doit(int fd);
 void parse_uri(char *uri,char *hostname,char *path,int *port);
 void build_http_header(char *http_header,char *hostname,char *path,int port,rio_t *client_rio);
-int connect_endServer(char *hostname,int port,char *http_header);
+int connect_endServer(char *hostname, int port);
 void *thread(void *vargsp);
+void init_cache(void);
+int reader(int connfd, char *url);
+void writer(char *url, char *buf);
+
+/* cache 구조체 */
+typedef struct {
+    char *url;      /* url 담을 변수 */
+    int *flag;      /* 캐시가 비어있는지 차 있는지 구분할 변수 */
+    int *cnt;       /* 최근 방문 순서 나타내기 위한 변수 */
+    char *content;  /* 클라이언트에 보낼 내용 담겨있는 변수 */
+} Cache_info;
+
+Cache_info *cache;  /* cache 변수 선언 */
+int readcnt;        /* 세마포어 cnt 할 변수 */
+sem_t mutex, w;     /**/
 
 /* end server info(브라우저 테스트를 위한) */
 // static const char *end_server_host = "localhost";   /* end server의 hostname은 현재 localhost */
@@ -40,25 +55,26 @@ int main(int argc, char **argv) {
         exit(1);
     }
     
+    init_cache();
     int listenfd, *connfd;                  /* listen 식별자, connfd 식별자 */
-    char hostname[MAXLINE], port[MAXLINE]; /* hostname: 접속한 클라이언트 ip, port: 접속한 클라이언트 port */
-    socklen_t clientlen;                   /* socklen_t 는 소켓 관련 매개 변수에 사용되는 것으로 길이 및 크기 값에 대한 정의를 내려준다 */
-    struct sockaddr_storage clientaddr;    /* 어떤 타입의 소켓 구조체가 들어올지 모르기 때문에 충분히 큰 소켓 구조체로 선언 */
-    pthread_t tid;
+    char hostname[MAXLINE], port[MAXLINE];  /* hostname: 접속한 클라이언트 ip, port: 접속한 클라이언트 port */
+    socklen_t clientlen;                    /* socklen_t 는 소켓 관련 매개 변수에 사용되는 것으로 길이 및 크기 값에 대한 정의를 내려준다 */
+    struct sockaddr_storage clientaddr;     /* 어떤 타입의 소켓 구조체가 들어올지 모르기 때문에 충분히 큰 소켓 구조체로 선언 */
+    pthread_t tid;                          /* thread ID */
 
     /* listenfd: 이 포트에 대한 듣기 소켓 오픈 */
     listenfd = Open_listenfd(argv[1]);
 
     while (1) {
-        clientlen = sizeof(clientaddr);                                             /* 소켓 구조체 크기 */
+        clientlen = sizeof(clientaddr);                                                 /* 소켓 구조체 크기 */
         connfd = Malloc(sizeof(int));
-        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                       /* 연결 요청 큐에 아무것도 없을 경우 기본적으로 연결이 생길때까지 호출자를 막아둠, 즉 대기 상태 */
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                      /* 연결 요청 큐에 아무것도 없을 경우 기본적으로 연결이 생길때까지 호출자를 막아둠, 즉 대기 상태 */
         Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); /* clientaddr: SA 구조체로 형변환, 소켓 정보를 가져옴 */
         printf("Accepted connection from (%s, %s)\n", hostname, port);                  /* 어떤 주소와 포트 번호를 가진 client가 들어왔는지 print */
         
         Pthread_create(&tid, NULL, thread, connfd);
 
-        // doit(connfd);                                                                   /* 트랜잭션 수행 */
+        // doit(connfd);                                                                /* 트랜잭션 수행 */
         
         /* 연결이 끝났다고 print 하고 식별자(파일 디스크립트)를 닫아줌 */
         // printf("endoffile\n");
@@ -77,20 +93,28 @@ void *thread(void *vargp) {
 
 void doit(int connfd) {
     int end_serverfd;
-
+    char content_buf[MAX_OBJECT_SIZE];
+    char url[MAXLINE];
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE]; /* buf: request 헤더 정보 담을 공간*/
     char endserver_http_header[MAXLINE];                                /* 서버에 보낼 헤더 정보를 담을 공간 */
     char hostname[MAXLINE], path[MAXLINE];                              /* hostname: IP담을 공간, path: 경로 담을 공간 */
     rio_t rio, server_rio;                                              /* rio: client rio 구조체, server_rio: proxy의 rio 구조체 */
     int port;                                                           /* port 담을 변수 */
 
+
     Rio_readinitb(&rio, connfd);                                            /* rio 구조체 초기화 */
     Rio_readlineb(&rio, buf, MAXLINE);                                  /* buf에 fd에서 읽을 값이 담김 */
     sscanf(buf, "%s %s %s", method, uri, version);                      /* sscanf는 첫 번째 매개 변수가 우리가 입력한 문자열, 두 번째 매개 변수 포맷, 나머지 매개 변수에 포맷에 맞게 데이터를 읽어서 인수들에 저장 */
+    strcpy(url, uri);
 
     /* GET이나 HEAD가 아닐 때 error 메시지 출력 */
-    if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD")) {
+    if (strcasecmp(method, "GET")) {
         printf("501 Not implemented Tiny does not implement this method");
+        return;
+    }
+
+    /* cache에서 찾았을 때 cache hit */
+    if(reader(connfd, url)) {
         return;
     }
 
@@ -99,7 +123,7 @@ void doit(int connfd) {
     /* 서버로 보낼 http header 만드는 함수 */
     build_http_header(endserver_http_header, hostname, path, port, &rio);
     /* 서버와 connect하는 함수 */
-    end_serverfd = connect_endServer(hostname, port, endserver_http_header);
+    end_serverfd = connect_endServer(hostname, port);
 
     /* 서버와 연결 실패 */
     if(end_serverfd < 0) {
@@ -114,10 +138,21 @@ void doit(int connfd) {
 
     /* server_rio의 내부 버퍼에 담긴 내용을 buf에 담고 클라이언트와 proxy에 연결되어 있는 식별자에 buf에 담긴 내용을 씀 */
     size_t n;
+    int total_size = 0;
     while((n = rio_readlineb(&server_rio, buf, MAXLINE)) != 0) {
         printf("proxy received %ld bytes,then send\n",n);
         Rio_writen(connfd, buf, n);
+        /* cache content의 최대 크기를 넘지 않으면 */
+        if (total_size + n < MAX_OBJECT_SIZE) {
+            strcpy(content_buf + total_size, buf);
+        }
+        total_size += n;
     }
+
+    if(total_size < MAX_OBJECT_SIZE) {
+        writer(url, content_buf);
+    }
+
     /* end_serverfd 닫기 */
     Close(end_serverfd);
 }
@@ -204,8 +239,90 @@ void build_http_header(char *http_header, char *hostname, char *path, int port, 
 }
 
 /* 서버와 proxy와 연결 */
-int connect_endServer(char *hostname, int port, char *http_header) {
+int connect_endServer(char *hostname, int port) {
     char portStr[100];
     sprintf(portStr, "%d", port);
     return Open_clientfd(hostname, portStr);
+}
+
+void init_cache() {
+    Sem_init(&mutex, 0, 1);
+    Sem_init(&w, 0, 1);
+    readcnt = 0;
+    cache = (Cache_info *)Malloc(sizeof(Cache_info) * 10);
+    for (int i = 0; i < 10; i++) {
+        cache[i].url = (char *)Malloc(sizeof(char) * 256);
+        cache[i].flag = (int *)Malloc(sizeof(int));
+        cache[i].cnt = (int *)Malloc(sizeof(int));
+        cache[i].content = (char *)Malloc(sizeof(char) * MAX_OBJECT_SIZE);
+        *(cache[i].flag) = 0;
+        *(cache[i].cnt) = 0;
+    }
+}
+
+int reader(int connfd, char *url) {
+    int return_flag = 0;
+    P(&mutex);
+    readcnt++;
+    if(readcnt == 1) {
+        P(&w);
+    }
+    V(&mutex);
+
+    for(int i = 0; i < 10; i++) {
+        if(*(cache[i].flag) == 1 && !strcmp(cache[i].url, url)) {
+            Rio_writen(connfd, cache[i].content, MAX_OBJECT_SIZE);
+            return_flag = 1;
+            *(cache[i].cnt) = 0;
+            break;
+        }
+    }    
+    
+    for(int i = 0; i < 10; i++) {
+        (*(cache[i].cnt))++;
+    }
+
+    P(&mutex);
+    readcnt--;
+    if(readcnt == 0) {
+        V(&w);
+    }
+    V(&mutex);
+    return return_flag;
+}
+
+void writer(char *url, char *buf) {
+    int cache_cnt = 0;
+    P(&w);
+
+    for(int i = 0; i < 10; i++) {
+        if(*(cache[i].flag) == 1 && !strcmp(cache[i].url, url)) {
+            cache_cnt = 1;
+            *(cache[i].cnt) = 0;
+            break;
+        }
+    }
+
+    if(cache_cnt == 0) {
+        int idx = 0;
+        int max_cnt = 0;
+        for(int i = 0; i < 10; i++) {
+            if(*(cache[i].flag) == 0) {
+                idx = i;
+                break;
+            }
+            if(*(cache[i].cnt) > max_cnt) {
+                idx = i;
+                max_cnt = *(cache[i].cnt);
+            }
+        }
+        *(cache[idx].flag) = 1;
+        strcpy(cache[idx].url, url);
+        strcpy(cache[idx].content, buf);
+        *(cache[idx].cnt) = 0;
+    }
+    for(int i = 0; i < 10; i++) {
+        (*(cache[i].cnt))++;
+    }
+    V(&w);
 }
